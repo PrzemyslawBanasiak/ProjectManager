5b7dedd7ea92132a503b575bbdf4fa7ae1f6dce1
Mateusz Debski <mateuszdeb@gmail.com>
2015-06-09 01:15:28 +0200

    addded require to project_helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,19 +1,34 @@
+require 'googlecharts'
+require 'gchart'
+
 module ProjectsHelper
   def pie_chart_for_tasks
     count = {}
-    total = 0
-    Task.all.each do |task|
-      if  task.project_id == @project .id
+
+    Task.where("project_id == ?", @project .id.to_s).each do |task|
         count[task.user] ||= 0
         count[task.user] += 1
-        total += 1
-      end
     end
 
+    total = count.count
     data = count.collect { |user, number| number }
     labels = count.collect { |user, number| "#{user.fullName} (#{total == 0 ? 0 : (100.0 * number / total).round}%)" }
 
     Gchart.pie(:data => data, :labels => labels, :size => '550x300', :theme => :thirty7signals)
   end
 
+def burndown_chart_of_project
+    count = {}
+    total = 0
+    Task.where("project_id == ?", @project .id.to_s).each do |task|
+        count[task.task_state] ||= 0
+        count[task.task_state] += 1
+        total += 1
+    end
+
+    data = count.collect { |state, number| number }
+    labels = count.collect { |state, number| "#{state} (#{total == 0 ? 0 : number })" }
+
+    Gchart.line(:data => data, :labels => labels, :size => '550x300', :theme => :thirty7signals)
+  end
 end
