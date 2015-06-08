9b14e2676bac849909f101c0d9420debdcad0f27
Przemyslaw Banasiak <200299@student.pwr.edu.pl>
2015-06-08 20:50:33 +0200

    Updated Task view/edit. Removed additional properties(view only)

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,21 @@ class Task < ActiveRecord::Base
   validates :name,  presence: true, uniqueness: true, length: { maximum: 50 }
   validates :description,  presence:true
   validates :project_id,  presence:true
+
+  before_save :default_values
+  def default_values
+    self.task_state  ||= 0
+  end
+
+  def task_state_name
+    if self.task_state == 0
+      return "To Do"
+    elsif self.task_state == 1
+      return "In Progess"
+    elsif self.task_state == 2
+      return "Done"
+    else
+      return "Undefined"
+    end
+  end
 end
